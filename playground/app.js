f258fb4c5b18e65e7e08b94ac3bbb805fa35c45c
Eyder ACM <eyderacm@gmail.com>
2019-02-17 21:59:33 -0600

    display fetched url

--- playground/app.js
+++ playground/app.js
@@ -2,4 +2,5 @@ const fetch = require('node-fetch');
 
 fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=3pGwJokbqy2JgZZMlYOMfjHpYtMWxqyk&location=1301%20lombard%20street%20philadelphia`)
 .then(response => response.json())
-.then(data => console.log(data));
+.then(data => console.log(JSON.stringify(data,  undefined, 2)))
+.catch(error => console.log(error));
